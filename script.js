c335abdf174948b059227b71cd15400d6b3c3f1d
naren <vnarendira71022@gmail.com>
2023-09-18 15:32:50 +0530

    18.9.23

---
+++ script.js
@@ -0,0 +1,9 @@
+var container=document.createElement("div");
+container.setAttribute("class","container");
+var row=document.createElement("div");
+row.setAttribute("class","row");
+var col=document.createElement("div");
+col.setAttribute("class","col");
+row.append(col,col2);
+container.append(row);
+document.body.append(div);
